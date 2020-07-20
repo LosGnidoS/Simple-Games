a75966b618cf2ba6dc788a6dc78897993b751cd4
LosGnidoS <64615865+LosGnidoS@users.noreply.github.com>
2020-07-20 22:19:01 +0300

    Add files via upload

---
+++ Dragons.py
@@ -0,0 +1,53 @@
+import random
+import time
+
+def displayIntro():
+    print(''' Вы находитесь в землях, заселенных драконами.
+перед собой вы видите  две пещеры. В одной из них - дружелюбный дракон,
+который готов поделиться с вами своими сокровищами. Во второй 
+- жадный и голодный дракон, который мигом вас съест.''')
+    print()
+
+
+def chooseCave():
+    cave = ''
+    while cave !='1' and cave !='2':
+        print('В какую пещеру вы войдете (нажмите клавишу 1 или 2)')
+        cave = input()
+
+
+    return cave
+
+
+def checkCave(chosenCave):
+    print('Вы приближаетесь к пещере...')
+    time.sleep(2)
+    print('Ее темнота заставляет дрожать вас в страхе...')
+    time.sleep(2)
+    print('Большой дракон выпригивает перед вами! Он раскрывает свою пасть и...')
+    print()
+    time.sleep(2)
+
+    friendlyCave = random.randint(1,2)
+
+
+
+
+    if chosenCave  == str(friendlyCave):
+        print('...делится с вами своими сокровищами')
+    else:
+        print('...моментально вас съедает')
+
+
+
+playAgain = 'да'
+while playAgain == 'да':
+    displayIntro()
+    caveNumber  = chooseCave()
+    checkCave(caveNumber)
+
+    print('try')
+    playagain = input()
+
+
+    
